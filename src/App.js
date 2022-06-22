16fefd7aa394a95dc5adb7069d9e646e658682f9
Rachel <rachel.gray7448@gmail.com>
2022-06-21 23:16:38 -0400

    turned contact form to single page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import ContactForm from './components/Contact';
 
 
 function App() {
+  const [contactSelected, setContactSelected] = useState(false);
+
   const [categories] = useState([
     {
       name: 'commercial',
@@ -24,11 +26,18 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
           <ContactForm></ContactForm>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
+        )}
       </main>
     </div>
   );
